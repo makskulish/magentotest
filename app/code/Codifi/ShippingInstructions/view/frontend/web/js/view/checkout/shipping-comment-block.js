532d6fb36d4270459d6a0cf010fe0e9bb1440e2b
mkulish <mkulish@codifi.me>
2021-08-10 14:45:01 +0300

    Created module for Shipping Instructions

---
+++ app/code/Codifi/ShippingInstructions/view/frontend/web/js/view/checkout/shipping-comment-block.js
@@ -0,0 +1,12 @@
+define([
+    'ko',
+    'uiComponent'
+], function (ko, Component) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'Codifi_ShippingInstructions/checkout/shipping-comment-block'
+        }
+    });
+});
