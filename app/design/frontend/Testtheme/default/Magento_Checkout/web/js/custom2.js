6289c52b823dafbc1512175567ecd578f31c04f5
mkulish <mkulish@codifi.me>
2021-08-12 14:19:04 +0300

    Edited files

--- app/design/frontend/Testtheme/default/Magento_Checkout/web/js/custom2.js
+++ app/design/frontend/Testtheme/default/Magento_Checkout/web/js/custom2.js
@@ -2,11 +2,19 @@ require([
     "jquery","jquery/ui"
 ], function($){
     $(document).ready(function() {
-
-
-        $(document).on("click","#click-me2", function() {
-            $('.comment-section').hide();
+        $(document).on("click","#help-shipping", function() {
+            $('#onepage-checkout-shipping-method-additional-load').show();
+            $('#onepage-checkout-shipping-method-additional-load').addClass('popup-bg');
+            $('<div class="body-color-active"></div>').appendTo('.checkout-index-index');
         });
 
+        $(document).mouseup(function (e){
+            var div = $("#onepage-checkout-shipping-method-additional-load");
+            if (!div.is(e.target)
+                && div.has(e.target).length === 0) {
+                div.hide();
+                $('.body-color-active').hide();
+            }
+        });
     });
 });
