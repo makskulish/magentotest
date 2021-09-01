2708bbbff50aa600c4103862a630d29721ce398a
mkulish <mkulish@codifi.me>
2021-09-01 18:56:40 +0300

    Edited file

--- app/code/Codifi/ShippingInstructions/view/frontend/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    config: {
-        mixins: {
-            'Magento_Checkout/js/view/shipping': {
-                'Codifi_ShippingInstructions/js/view/shipping': true
-            }
-        }
-    }
-};
